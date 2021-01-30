a5a296fcf46c3dc8d4a65f2d0d43164296f94add
Abhay Patil <Abhay Patil@DESKTOP-J6ILB0P>
2021-01-30 12:11:56 +0530

    start printing done

---
+++ testing github/src/testing_github/StarPrinting.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package testing_github;
+
+/**
+ * @author Abhay Patil
+ *
+ */
+public class StarPrinting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+		
+		
+	}
+
+}
